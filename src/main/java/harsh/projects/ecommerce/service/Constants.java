729ffb32b4950060d99af0647f3be3abc79f9754
Harsh Patel <46072683+Harsh-B-Patel@users.noreply.github.com>
2025-01-12 21:07:21 -0500

    Update

--- src/main/java/harsh/projects/ecommerce/service/Constants.java
+++ src/main/java/harsh/projects/ecommerce/service/Constants.java
@@ -2,10 +2,16 @@ package harsh.projects.ecommerce.service;
 
 public class Constants {
 	
-	public static String DB_URL = "jdbc:mysql://database-1.clq82oum6pvk.us-east-1.rds.amazonaws.com:3306/ecommerce";
-    public static String DB_USER = "admin"; 
+	
+	// Test Env
+	//public static String DB_URL = "jdbc:mysql://localhost:3306/ecommerce";
+	//public static String DB_Root_URL = "jdbc:mysql://localhost:3306";
+	
+	
+	public static String DB_URL = "jdbc:mysql://database.clq82oum6pvk.us-east-1.rds.amazonaws.com:3306/ecommerce";
+	public static String DB_Root_URL = "jdbc:mysql://database.clq82oum6pvk.us-east-1.rds.amazonaws.com:3306";
+    public static String DB_USER = "root"; 
     public static String DB_PASSWORD = "RWNvbW1lckFQSUVjb21tZXJBUElFY29tbWVyQVBJ";
-    
     public static String SUBJECT = "EcommerceAPIUser";
     
 }
